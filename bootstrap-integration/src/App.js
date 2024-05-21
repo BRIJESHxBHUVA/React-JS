fe2881ffb8a060da381f165c1e1eddba2cdbd6d9
BRIJESHxBHUVA <bhuvabrijesh14@gmail.com>
2024-05-21 17:40:29 +0530

    bootstrap-integration

---
+++ bootstrap-integration/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Header from './Components/Header';
+import Section from './Components/Section-1';
+import Section2 from './Components/Section-2';
+import Section3 from './Components/Section-3';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+    <Section/>
+    <Section2/>
+    <Section3/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
